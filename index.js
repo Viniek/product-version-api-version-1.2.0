d7ed6f260d08197f2861f44a8dc940e6528699be
unknown <njerivictory52@gmail.com>
2024-06-26 10:03:58 +0300

    --

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
-
+import productsRouter from './routes/products.routes.js';
 const app=express();
 
-app.listen(3000,()=>{
+app.use("/products",productsRouter)
+
+app.listen(3008,()=>{
     console.log("app running on port 3000....");
 })
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,18 +1,19 @@
 import { Router }from 'express';
 
-const router=Router;
+const router=Router();
 router.get('/',(req,res)=>{
-    res.send("getting all products")
+    res.send("getting all products...")
 })
 router.get("/:id",(req,res)=>{
-    res.send("getting a single product")
+    res.send("getting a single product...")
 })
 router.post("/:id",(req,res)=>{
-    res.send("creating a single product")
+    res.send("creating a single product...")
 })
-router.patch("/",(req,res)=>{
-    res.send("updating a product")
+router.patch("/:id",(req,res)=>{
+    res.send("updating a single product...")
 })
 router.delete("/:id",(reg,res)=>{
-    res.send("deleting a single product")
+    res.send("deleting a single product...")
 })
+export default router;
